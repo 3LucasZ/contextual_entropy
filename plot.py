59c00553bad5cf66c73b7c1a67c35bffdb5cfb3a
3LucasZ <lucas.zheng@warriorlife.net>
2025-12-08 16:26:21 -0800

    experiment 3 plots

--- plot.py
+++ plot.py
@@ -14,6 +14,10 @@ with open('data/exp2.json') as f2:
 df2 = pd.DataFrame(list(results_2.items()), columns=['Context', 'Entropy'])
 # df2 = df2.sort_values('Entropy', ascending=False)
 
+with open('data/exp3.json') as f3:
+    results_3 = json.load(f3)
+df3 = pd.DataFrame(list(results_3.items()), columns=['Context', 'Entropy'])
+
 # Plot 1
 plt.figure(figsize=(10, 6))
 sns.barplot(data=df1, y='Context', x='Entropy', palette='viridis')
@@ -34,3 +38,13 @@ plt.axvline(x=results_2["[]"], color='r', linestyle='--', label='Baseline')
 plt.legend()
 plt.tight_layout()
 plt.savefig("entropy2.jpg")
+
+# Plot 3
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df3, y='Context', x='Entropy', palette='viridis')
+plt.title('Experiment 3', fontsize=14)
+plt.xlabel('Conditional Entropy', fontsize=12)
+plt.axvline(x=results_3["[]"], color='r', linestyle='--', label='Baseline')
+plt.legend()
+plt.tight_layout()
+plt.savefig("entropy3.jpg")
